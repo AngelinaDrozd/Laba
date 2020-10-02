045e4ce8bcdd780115fddb7378062222f938d4f6
USER <19fi.a.drozd@std.npu.edu.ua>
2020-10-02 08:38:02 +0300

    Changing

--- src/main/java/ua/edu/npu/task01/App.java
+++ src/main/java/ua/edu/npu/task01/App.java
@@ -6,23 +6,21 @@ public class App {
     public static void main(String[] args) {
         Semaphore book = new Semaphore(3);
 
-        Readers reader = new Readers();
         Readers reader1 = new Readers();
         Readers reader2 = new Readers();
         Readers reader3 = new Readers();
         Readers reader4 = new Readers();
 
-        reader.book = book;
         reader1.book = book;
         reader2.book = book;
         reader3.book = book;
         reader4.book = book;
 
-        reader.start();
         reader1.start();
         reader2.start();
         reader3.start();
         reader4.start();
+
 
     }
 }
